8a52c67b526448823252147d688e5bf22724b7f4
Uallace Macedo <uallace.macedo@outlook.com>
2025-05-07 16:39:50 -0300

    feat: autentication

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth.middleware.js';
 
 import SessionController from './app/controllers/session.controller.js';
 
 const routes = new Router();
 routes.post('/session', SessionController.session);
+
+routes.use(authMiddleware);
+routes.get('/test', (req, res) => {
+  res.status(200).json({ cnpj: req.cnpjCompany, id: req.idCompany });
+})
 
 export default routes;
